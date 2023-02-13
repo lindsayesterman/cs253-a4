44748561ca6a0e4fa8dc3b7ec3f037e673753e35
Lindsay Esterman <lindsayesterman@gmail.com>
2023-02-13 12:04:25 -0500

    quicksort

--- myEfficientSort.java
+++ myEfficientSort.java
@@ -2,43 +2,47 @@ class myEfficientSort {
     static int comparisons = 0;
     static int swaps = 0;
 
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
     static int[] myEfficientSort(int[] a) {
-        int[] h = new int[a.length];
-        myEfficientSort(a, 0, a.length - 1, h);
-        System.out.println("Number of comparisons " + comparisons);
+        myEfficientSort(a, 0, a.length - 1);
+        System.out.println("Number of comparisons: " + comparisons);
+        System.out.println("Number of swaps: " + swaps);
         return a;
     }
 
-    static void myEfficientSort(int[] a, int l, int r, int[] h) {
-        if (l < r) {
-            int q = (l + r) / 2;
-            myEfficientSort(a, l, q, h);
-            myEfficientSort(a, q + 1, r, h);
-            merge(a, l, q, r, h);
-        }
-    }
-
-    static void merge(int[] a, int l, int q, int r, int[] h) {
-        int i, j;
-        for (i = l; i <= q; i++)
-            h[i] = a[i];
-        for (j = q + 1; j <= r; j++)
-            h[r + q + 1 - j] = a[j];
-        i = l;
-        j = r;
-        for (int k = l; k <= r; k++) {
-            if (h[i] <= h[j]) {
-                a[k] = h[i];
+    static void myEfficientSort(int[] a, int left, int right) {
+        int i = left;
+        int j = right;
+        int pivot = a[(i + j) / 2];
+        while (i <= j) {
+            while (a[i] < pivot) {
                 i++;
-            } else {
-                a[k] = h[j];
+                comparisons++;
+            }
+            while (a[j] > pivot) {
+                j--;
+                comparisons++;
+            }
+            if (i <= j) {
+                swap(a, i, j);
+                swaps++;
+                i++;
                 j--;
             }
         }
+        if (left < j)
+            myEfficientSort(a, left, j);
+        if (i < right)
+            myEfficientSort(a, i, right);
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] { 1, 3, 5, 7, 2, 4, 6, 8 };
+        int[] arr = new int[] { 4, 1, 3, 2, 7, 6, 5, 8 };
         // for (int i = 0; i < arr.length; i++) {
         // System.out.print(arr[i] + " ");
         // }
